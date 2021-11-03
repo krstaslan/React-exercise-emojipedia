10db90aee277f211c5614a552846a7da53ae362f
krstaslan <63463164+krstaslan@users.noreply.github.com>
2021-11-03 23:57:49 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Emoji from "./Emoji";
+import emojipedia from "../emojipedia";
+
+function createEmoji(emojipedia) {
+  return (
+    <Emoji
+      id={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+    </div>
+  );
+}
+
+export default App;
